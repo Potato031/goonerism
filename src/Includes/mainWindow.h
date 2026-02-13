892eddd635c8ce088d4c900af7b4ece5062152bb
Potato031 <potato03160@gmail.com>
2026-02-13 12:27:16 -0500

    Add Linux support and finalize multi-platform auto-updater

--- src/Includes/mainWindow.h
+++ src/Includes/mainWindow.h
@@ -24,7 +24,7 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
 public:
     MainWindow(QWidget *parent = nullptr);
-    const QString CURRENT_VERSION = "v1.0.9";
+    const QString CURRENT_VERSION = "v1.0.11";
     void downloadUpdate(const QString &url);
     void finalizeUpdate();
     void checkForUpdates();
