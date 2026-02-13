2927a3310efcfa19313ff0eb58db2228ae0c353a
Potato031 <potato03160@gmail.com>
2026-02-13 18:41:21 -0500

    Fix Windows update loop logic

--- src/Includes/mainWindow.h
+++ src/Includes/mainWindow.h
@@ -24,7 +24,7 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
 public:
     MainWindow(QWidget *parent = nullptr);
-    const QString CURRENT_VERSION = "v1.0.23";
+    const QString CURRENT_VERSION = "v1.0.24";
     void downloadUpdate(const QString &url);
     void finalizeUpdate();
     void checkForUpdates();
